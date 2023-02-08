b98e5b3de26eca9c1cd2893c770ae9784fbc7373
Johnny Chan <chanyao@umich.edu>
2023-02-08 15:42:18 +0800

    Update packet_validator.cc

--- packet_validator.cc
+++ packet_validator.cc
@@ -2,6 +2,41 @@
 <src_ip>:<dst_ip>:<message_type>[:<payload>]
 ******/
 
+/**********************************************************
+#ifndef __TIMER_HPP__
+#define __TIMER_HPP__
+#include <chrono>
+using hr_clock = std::chrono::high_resolution_clock();
+using sec = std::chrono::seconds;
+using tp = hr_clock::time_point;
+class Timer {
+public:
+
+  void start()
+  {
+    last_start = hr_clock::now();
+    last_checkpoint = last_start;
+  }
+  template <typename ResolutionType = sec>
+  ResolutionType checkpoint()
+  {
+    tp now(hr_clock::());
+    ResolutionType diff = std::chrono::duration_cast<ResolutionType>(now - last_checkpoint);
+    last_checkpoint = now;
+    return diff; // remember to call count() when trying to print result
+  }
+  template <typename ResolutionType = sec>
+  ResolutionType stop()
+  {
+    return std::chrono::duration_cast<ResoltuionType>(hr_clock::now() - last_start);
+  }
+private:
+  tp last_start;
+  tp last_checkpoint;
+};
+#endif
+**********************************************************/
+
 class packet_validator
 {
 public:
@@ -21,4 +56,9 @@ public:
   // periodically called often enough for the validator to detect connection timeouts to properly handle further packets
   // return: number of connection timeouts detected in this call
   int handle_timeouts(time_point now);
+
+  
+private:
+
+
 };
